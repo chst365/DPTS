038006bb1879c80a25c2723def68212c906875d6
chst365 <chstz_365@163.com>
2018-04-11 18:29:59 +0800

    add
    Signed-off-by: chst365 <chstz_365@163.com>

---
+++ src/views/Main.vue
@@ -0,0 +1,19 @@
+<template>
+	<div class="con-box">
+        <div class="center-box">
+            <!-- <p>一点链接，协同所有</p>  -->
+            <img src="../assets/images/logo-h.png">
+        </div>
+        <p class="tit">树袋老师CRM意向学生系统</p>
+    </div>
+</template>
+
+<script>
+	export default {
+	}
+
+</script>
+
+<style>
+@import "../assets/css/1a0.css"
+</style>
